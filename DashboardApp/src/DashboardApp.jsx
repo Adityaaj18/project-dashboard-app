ba64ceb86028bffb5432433259f516254b3ee0b8
Aditya <39948420+Adityaaj18@users.noreply.github.com>
2025-12-19 06:54:16 +0000

    initial

---
+++ DashboardApp/src/DashboardApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const DashboardApp = () => {
+ 
+
+  return (
+    <div className="app">
+      <div className="header">
+        {console.log('ddsd')
+        }
+        <h2>Dashboard</h2>
+      </div>
+      
+    </div>
+  );
+};
+
+export default DashboardApp;
